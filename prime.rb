4e9998f20cad5fdf277943224f149d49eefd7860
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-11 03:47:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
 def prime?(number)
-    if number > 1
-      (2..number - 1).all? do |x|
-        number % x != 0
-      end
-    else
-      return false
-    end
+  if number > 1
+    (2..number - 1).all? {|x| number % x != 0}
+  else
+    return false
+  end
 end
